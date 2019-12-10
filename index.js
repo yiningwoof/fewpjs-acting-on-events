86d7194ebcf1596bea94e864b95ed5c3b99bc67f
Yining Wang <yining.wang@utexas.edu>
2019-12-10 17:23:51 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Your code here
+
+let dodger = document.getElementById("dodger");
+// dodger.style.backgroundColor = "#000000";
+dodger.style.backgroundColor = "#FF69B4";
+
+dodger.style.left; // "180px"
+dodger.style.bottom; // "0px"
+
+dodger.style.bottom = "100px";
+
+dodger.style.bottom = "0px";
+
+// document.addEventListener("keydown", function(e) {
+//     console.log(e.key);
+// });
+
+// document.addEventListener("keydown", function(e) {
+//     if (e.key === "ArrowLeft") {
+//         let leftNumbers = dodger.style.left.replace("px", "");
+//         let left = parseInt(leftNumbers, 10);
+
+//         dodger.style.left = `${left - 1}px`;
+//     }
+// });
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+});
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+});
